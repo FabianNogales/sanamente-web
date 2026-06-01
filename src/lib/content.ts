53aef2f489bf35d7349f70eca6ae5188f9f52166
Jhaseft <jhasesaat@gmail.com>
2026-06-01 11:53:43 -0400

    cambio en imagen

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,3 +1,3 @@
 // URLs de contenido — pon tus links aquí
-export const IMAGEN_HERO = "https://res.cloudinary.com/dnbklbswg/image/upload/v1780271507/HC4SD_boauxu.jpg"; // URL de la imagen del hero (lado derecho)
+export const IMAGEN_HERO = "https://res.cloudinary.com/dnbklbswg/image/upload/v1780328867/WhatsApp_Image_2026-06-01_at_11.27.49_dym1ll.jpg"; // URL de la imagen del hero (lado derecho)
 export const VIDEO_INICIO = "https://res.cloudinary.com/dnbklbswg/video/upload/v1780270324/WhatsApp_Video_2026-05-30_at_16.46.36_aacsj5.mp4"; // URL del video introductorio MP4
